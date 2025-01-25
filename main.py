afe2ff468f0c50bb1ce66435b24cdfc0be8df0df
marcusayay <marcusayay@gmail.com>
2025-01-25 15:40:47 +0800

    V5.5

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 app = Flask(__name__)
 
 
